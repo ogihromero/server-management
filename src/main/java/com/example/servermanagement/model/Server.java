d20410eff30d60e64294f26af7f06c7485125b8e
Adriano Romero <adriano.romerofco@gmail.com>
2022-10-16 15:41:37 -0300

    Initial commit

---
+++ src/main/java/com/example/servermanagement/model/Server.java
@@ -0,0 +1,9 @@
+package com.example.servermanagement.model;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String memory;
+}
